124046ccc641761de62dcc30c13c1fc932fdce6b
Mona <mona.alshlowi@gmail.com>
2023-02-15 22:21:05 +0300

    fix lint

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://udagram-api-dev.eba-3dvw8mm5.us-east-1.elasticbeanstalk.com/api/v0",
+  appName: 'Udagram',
+  apiHost: 'http://udagram-api-dev.eba-3dvw8mm5.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: "http://udagram-api-dev.eba-3dvw8mm5.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: 'http://udagram-api-dev.eba-3dvw8mm5.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
